8c6f279932e8248e335119738b1a1610d93514f0
Rebeca <rebecagomesdev@gmail.com>
2024-09-14 17:04:00 -0300

    menu do app

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-function start () {
-    console.log('comecou')
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+
+    while(true){
+        
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case"sair":
+                console.log("Até mais!")
+                return
+        }
+    }
 }
 
 start ()
